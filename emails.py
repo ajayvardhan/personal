3c261f643b84454646ef9f1dcdc04e6501df40f1
ajayvardhan <ajyvardan@gmail.com>
2018-11-08 20:36:46 -0500

    Granular functions

--- emails.py
+++ emails.py
@@ -1,10 +1,10 @@
 import email
 from os import walk
 from collections import OrderedDict
-from sets import Set
 from dateutil.parser import parse
-import pprint
+import unittest
 
+# email object to store each email along with it's parent
 class Email:
 
     def __init__(self, message_id, reply_to, date, from_address, to_addresses, subject):
@@ -16,69 +16,85 @@ class Email:
         self.subject = subject
         self.parent = None
 
-
-def get_parent_reply_to(mail, mails):
+# get the parent of "in reply to"
+def reply_to_parent(mail, mails):
     for m in mails:
         if mail.reply_to == m.message_id:
             return m
 
-def get_parent_subject(mail, mails):
+# get parent for the same subject and the from and to addresses match
+def subject_parent(mail, mails):
     for m in mails:
         if mail.subject == m.subject and mail.from_address in m.to_addresses and m.from_address in mail.to_addresses:
             return m
 
+# parse the subject out of the email
 def get_subject(mail):
     subject = mail['subject'].lower()
     if "fw" in subject.split(":"):
         return subject
     return subject.split(":")[-1].strip()
 
+# parse the emails and create email objects for each email
+def parse_emails(directory):
+    for (dirpath, dirnames, filenames) in walk(directory):
+        emails = filenames
+    mails = OrderedDict()
+    unsorted_mails = {}
+    conversations = OrderedDict()
+    visited = {}
+    email_list = []
 
-for (dirpath, dirnames, filenames) in walk("raw_emails"):
-    emails = filenames
-mails = OrderedDict()
-unsorted_mails = {}
-conversations = OrderedDict()
-visited = {}
-email_list = []
+    for mail in emails:
+        message = open(directory + "/" + mail, "r").read()
+        msg = email.message_from_string(message)
 
-for mail in emails:
-    message = open("raw_emails/" + mail, "r").read()
-    msg = email.message_from_string(message)
+        msg_subject = get_subject(msg)
 
-    msg_subject = get_subject(msg)
+        email_item = Email(msg["Message-ID"],
+                           msg['In-Reply-To'],
+                           parse(msg['Date']),
+                           msg["From"],
+                           msg["To"].split(', '),
+                           msg_subject)
+        first_parent = reply_to_parent(email_item, email_list)
+        second_parent = subject_parent(email_item, email_list)
+        if first_parent:
+            email_item.parent = first_parent
+        elif second_parent:
+            email_item.parent = second_parent
 
-    email_item = Email(msg["Message-ID"],
-                       msg['In-Reply-To'],
-                       parse(msg['Date']),
-                       msg["From"],
-                       msg["To"].split(', '),
-                       msg_subject)
-    first_parent = get_parent_reply_to(email_item, email_list)
-    second_parent = get_parent_subject(email_item, email_list)
-    if first_parent:
-        email_item.parent = first_parent
-    elif second_parent:
-        email_item.parent = second_parent
-
-    email_list.append(email_item)
+        email_list.append(email_item)
 
 
-email_list.sort(key=lambda x: x.date, reverse=True)
+    email_list.sort(key=lambda x: x.date, reverse=True)
+    return email_list
 
-def print_thread(mail, thread):
+# create thread for each email. DFS traversal the email until we each the topmost parent
+def get_thread(mail, thread):
     thread.append(mail.message_id)
     if not mail.parent:
         return thread
     else:
-        return print_thread(mail.parent, thread)
+        return get_thread(mail.parent, thread)
 
-visited = []
-for mail in email_list:
-    thread = print_thread(mail, [])
-    if mail.message_id not in visited:
-        print print_thread(mail, [])
-    visited.extend(thread)
+# get the thread for each email
+def get_conversations(email_list):
+    conversations = []
+    visited = []
+    for mail in email_list:
+        thread = get_thread(mail, [])
+        if mail.message_id not in visited:
+            conversations.append(thread)
+        visited.extend(thread)
+    return conversations
+
+# main program
+emails = parse_emails("raw_emails")
+conversations = get_conversations(emails)
+# for c in conversations:
+#     print c
+
 
 # Output:
 # ['<17@example>']
